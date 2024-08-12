dd1ace3cca90f59d83c186d516214035f14a253b
Bruno D'Luka <brunodlukaa@gmail.com>
2024-08-12 10:38:14 -0300

    feat: Create script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# programacao_web_cc_a
+# Programação Web
+
 College project. Repositório da disciplina de Programação Web do Curso de Ciência da Computação turma A - CEUB.
---
+++ script.js
@@ -0,0 +1,12 @@
+let button = document.getElementById("ok")
+button.onclick = () => {
+    let nomeInput = document.getElementById("nome-input")
+    console.log(nomeInput.value)
+
+    let idadeInput = document.getElementById("idade-input")
+    console.log(idadeInput.value)
+
+    let emailInput = document.getElementById("email-input")
+    console.log(emailInput.value)
+}
+
