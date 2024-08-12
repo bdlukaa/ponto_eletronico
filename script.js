388d4796e4bbe8f95bd179ac9db1175fdfe6cb07
Bruno D'Luka <brunodlukaa@gmail.com>
2024-08-12 10:49:42 -0300

    feat: Validae email input

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let button = document.getElementById("ok")
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
 button.onclick = () => {
     let nomeInput = document.getElementById("nome-input")
     console.log(nomeInput.value)
@@ -7,6 +15,9 @@ button.onclick = () => {
     console.log(idadeInput.value)
 
     let emailInput = document.getElementById("email-input")
+    if (!validateEmail(emailInput.value)) {
+        alert("Email is not valid")
+    }
     console.log(emailInput.value)
 }
 
