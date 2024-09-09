a7f5d1d9335635cb064fda8e4e8e40590108111a
Bruno D'Luka <brunodlukaa@gmail.com>
2024-09-09 08:42:28 -0300

    chore: Ponto Eletronico

---
+++ js/index.js
@@ -0,0 +1,74 @@
+navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+});
+
+
+const diaSemana = document.getElementById("dia-semana");
+const dataAtual = document.getElementById("data-atual");
+const horaAtual = document.getElementById("hora-atual");
+const btnRegistrarPonto = document.getElementById("btn-registrar-ponto");
+
+btnRegistrarPonto.addEventListener("click", register);
+
+diaSemana.textContent = getWeekDay();
+dataAtual.textContent = getCurrentDate();
+
+
+const dialogPonto = document.getElementById("dialog-ponto");
+
+const dialogData = document.getElementById("dialog-data");
+dialogData.textContent = getCurrentDate();
+
+const dialogHora = document.getElementById("dialog-hora");
+dialogHora.textContent = getCurrentTime();
+
+
+const btnDialogEntrada = document.getElementById("btn-dialog-entrada");
+btnDialogEntrada.addEventListener("click", () => {
+    // recuperar informações 
+    // data, hora, localização (lat, long), tipo: entrada
+    // salvar essas informações num objeto Javascript
+})
+
+const btnDialogFechar = document.getElementById("dialog-fechar");
+btnDialogFechar.addEventListener("click", () => {
+    dialogPonto.close();
+})
+
+function register() {
+    dialogPonto.showModal();
+}
+
+
+function updateContentHour() {
+    horaAtual.textContent = getCurrentTime();
+}
+
+// Retorna a hora atual (hora/minuto/segundo)
+function getCurrentTime() {
+    const date = new Date();
+    return String(date.getHours()).padStart(2, '0') + ":" + String(date.getMinutes()).padStart(2, '0') + ":" + String(date.getSeconds()).padStart(2, '0');
+}
+
+// Retorna a data atual no padrão dd/mm/aaaa
+function getCurrentDate() {
+    const date = new Date(); 
+    let mes = date.getMonth() + 1;
+    return String(date.getDate()).padStart(2, '0') + "/" + String(mes).padStart(2, '0') + "/" +  String(date.getFullYear()).padStart(2, '0');
+}
+
+function getWeekDay() {
+    const date = new Date()
+    const day = date.getDay()
+    const daynames = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+    return daynames[day]
+}
+
+updateContentHour();
+setInterval(updateContentHour, 1000);
+
+console.log(getCurrentTime());
+console.log(getCurrentDate());
+console.log(getWeekDay());
